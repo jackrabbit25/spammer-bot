302e80aa6451f9ee846aca47b21173b3afb55ed5
3pehrdev <sepehrsalehi0111@gmail.com>
2016-03-16 22:56:16 +0330

    spam with 1 messages (!/#spam)

---
+++ plugins/spammer1.lua
@@ -0,0 +1,28 @@
+do
+
+function run(msg, matches)
+       if not is_sudo(msg) then
+              return "sik baw Only Sudo Can Use it"       end
+  local tex = matches[1]
+  local sps = matches[2]
+  local sp = 'done by BlackLifeTM'
+
+for i=1, tex, 1 do
+
+sp = '\n'..sps..'\n'..sp
+i = i + 1
+
+end
+
+return sp
+
+end
+
+return {
+    patterns = {
+      "[!/#]spam (.*) (.*)$"
+    },
+    run = run,
+}
+
+end
