d0feb3d9f4ee2f376ce0754e435f06f8a322a596
3pehrdev <sepehrsalehi0111@gmail.com>
2016-03-17 03:24:50 +0330

    Update autoaddersudo.lua

--- plugins/autoaddersudo.lua
+++ plugins/autoaddersudo.lua
@@ -1,7 +1,7 @@
 do
 local function run(msg, matches)
 local bot_id = our_id 
-local fbot1 = hdnd o,nj,k, fckdn
+local fbot1 = idi khodetoono inja bezanid
     if msg.action.type == "chat_del_user" and msg.action.user.id == tonumber(fbot1) then
        chat_add_user("chat#id"..msg.to.id, 'user#id'..fbot1, ok_cb, false)
     end
