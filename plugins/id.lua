99167e95d5bda01ec6e5e110062d12885a0b5784
3pehrdev <sepehrsalehi0111@gmail.com>
2016-04-07 14:45:36 +0430

    Update id.lua

--- plugins/id.lua
+++ plugins/id.lua
@@ -1,14 +1,29 @@
-local function run(msg, matches ) 
-  if matches[1] == "id" then
-    return "⚠️Your id⚠️\n "..msg.from.id
-  end
+do function run(msg, matches)
+
+	  if matches[1]:lower() == 'id' and is_sudo(msg) then
+      return "Sudo☑\n⭕⭕️Your id⭕️\n"..msg.from.id.."\n⭕️Group id⭕️\n"..msg.to.id
+      end
+      if matches[1]:lower() == 'id' and is_admin(msg) then
+      return "Admin☑️\n⭕️Your id⭕️\n"..msg.from.id.."\n⭕️Group id⭕️\n"..msg.to.to.id
+      end
+      if matches[1]:lower() == 'id' and is_owner(msg) then
+      return "Owner☑️\n⭕️Your id⭕️\n"..msg.from.id.."\n⭕️Group id⭕️\n"..msg.to.to.id
+      end
+      if matches[1]:lower() == 'id' and is_momod(msg) then
+      return "Moderator☑️\n⭕️Your id⭕️\n"..msg.from.id.."\n⭕️Group id⭕️\n"..msg.to.id
+      end
+      if matches[1]:lower() == 'id' then
+      return "Member☑️\⭕️Your id⭕️\n"..msg.from.id.."\n⭕️Group id⭕️\n"..msg.to.id
+    end
+end
+  return {
+  description = "show your id",
+  usage = "id",
+  patterns = {
+  "^[!/#]([Ii][Dd])$",
+    },
+  run = run
+}
 end
 
-return {
-  patterns ={
-    "^[/! #]([Ii][Dd])$" 
- }, 
-  run = run 
-}
-
---by blacklifeTM
+--sepehr blacklifeTM
