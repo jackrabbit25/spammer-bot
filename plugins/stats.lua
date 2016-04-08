1cb4ef785fd3086208131199a4d592431929c3bb
3pehrdev <sepehrsalehi0111@gmail.com>
2016-04-08 13:57:00 +0430

    Update stats.lua

--- plugins/stats.lua
+++ plugins/stats.lua
@@ -139,7 +139,8 @@ end
 return {
 	description = "show stats bot"
 	usage = {
-		"!spammerbot : show information and stats for bot",
+		"!spammerbot : show information and stats for bot"
+		},
   patterns = {
     "^[!/]([Ss]tats)$",
     "^[!/]([Ss]tatslist)$",
