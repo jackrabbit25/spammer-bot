16b4da111a0d76227804c1e6f86e9344bb20d0ad
3pehrdev <sepehrsalehi0111@gmail.com>
2016-04-08 15:17:06 +0430

    Update feedback.lua

--- plugins/feedback.lua
+++ plugins/feedback.lua
@@ -1,42 +1,66 @@
 do
 
+
 function run(msg, matches)
 
-local fuse = '▪️DearAdmin , we have recive a new feedback just now : ▫️newfeedback \n\n▪️ID : ' .. msg.from.id .. '\n\n▫️Name : ' .. msg.from.print_name ..'\n\n▪️username : @' .. msg.from.username  ..'\n\n☑️FeedBack :\n\n\n' .. matches[1] 
+
+local fuse = '#DearAdmin😜 we have recived a new feedback just now : #newfeedback \n\nID▶️ : ' .. msg.from.id .. '\n\nName▶️ : ' .. msg.from.print_name ..'\n\nusername▶️ :@ ' .. msg.from.username ..'\n\n🅿️♏️ :\n\n\n' .. matches[1]
+
 local fuses = '!printf user#id' .. msg.from.id
 
 
+
     local text = matches[1]
+
  bannedidone = string.find(msg.from.id, '123')
-        bannedidtwo =string.find(msg.from.id, '465')       
-   bannedidthree =string.find(msg.from.id, '678')  
+
+        bannedidtwo =string.find(msg.from.id, '465')
+
+   bannedidthree =string.find(msg.from.id, '678')
+
 
 
         print(msg.to.id)
 
-        if bannedidone or bannedidtwo or bannedidthree then                    —for banned people
+
+        if bannedidone or bannedidtwo or bannedidthree then --for banned people
+
                 return 'You are banned to send a feedback'
+
  else
 
 
-                 local sends0 = send_msg('chat#129631380', fuse, ok_cb, false)
 
- return 'thank you"..msg.from.print_name.."\nyour feedback succsefulli send to Support Spammer-bot :)'
+                 local sends0 = send_msg('chat# 129631380 ', fuse, ok_cb, false)
 
-     
+
+ return 'your feedback succesfully recived to support spammer-bot
+
+
+
 
 end
 
+
 end
+
 return {
+
   description = "Feedback",
 
-  usage = "feedback : send maseage to admins with bot",
+
+  usage = "!feedback : send maseage to admins with bot",
+
   patterns = {
-    "^feedback (.*)$"
+
+    "^![Ff]eedback (.*)$"
+
 
   },
+
   run = run
+
 }
+
 
 end
