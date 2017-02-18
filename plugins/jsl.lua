c08a62c9b73e17b0479100b574ab55994ddbafbe
3pehrdev <sepehrsalehi0111@gmail.com>
2017-02-19 00:28:39 +0330

    Update jsl.lua

--- plugins/jsl.lua
+++ plugins/jsl.lua
@@ -1,20 +1,18 @@
 local function run(msg, matches)
 if matches[1] == "joinspam" and is_admin1(msg) then
-    local join = matches[2]
-     local link = matches[3]
-        local receiver = get_receiver(msg)
-        for i=1,join do
-  local hash = parsed_url(matches[3])
-      import_chat_link(hash,ok_cb,false)
- if msg.service and msg.action.type == "chat_add_user_link" and msg.action.user.id == tonumber(our_id) and not is_sudo(msg) then
-send_large_msg(receiver, 'DArkWeB Was Here . . .! FUCK BITCHS💀\n@BlackLifeTM',ok_cb, false)
- chat_del_user(receiver, 'user#id'..our_id, ok_cb, false)
+local join = matches[2]
+local link = matches[3]
+for i=1,join do
+import_chat_link(parsed_url(matches[3]),ok_cb,false)
+send_large_msg(get_receiver(msg), 'DArkWeB Was Here . . .! FUCK BITCHS💀\n@BlackLifeTM',ok_cb, false)
+chat_del_user(get_receiver(msg), 'user#id'..our_id, ok_cb, false)
+leave_channel(get_receiver(msg), ok_cb, false)
+end 
     end
-        end
-            
-    
-patterns = {
-  "[!#/](joinspam) (%d+) (.*)$",
-  },
-  run = run,
+end
+return {
+    patterns = {
+        "^[!#/](joinspam) (.*) (%d+)$"
+    },
+    run = run
 }
