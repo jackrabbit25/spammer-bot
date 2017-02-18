beb98c1f0ce6117fa519e3cb18e4ebd58c4231bd
3pehrdev <sepehrsalehi0111@gmail.com>
2017-02-19 00:48:51 +0330

    Update jsl.lua

--- plugins/jsl.lua
+++ plugins/jsl.lua
@@ -1,4 +1,9 @@
 local function run(msg, matches)
+  local function parsed_url(link)
+  local parsed_link = URL.parse(link)
+  local parsed_path = URL.parse_path(parsed_link.path)
+  return parsed_path[2]
+end
 if matches[1] == "joinspam" and is_admin1(msg) then
 local join = matches[2]
 local link = matches[3]
