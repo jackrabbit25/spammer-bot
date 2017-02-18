41b8704d032b19b39cba13fca21237064dd41e6c
3pehrdev <sepehrsalehi0111@gmail.com>
2017-02-18 18:02:16 +0330

    Send Spam To THe USer#id

---
+++ plugins/spammer4.lua
@@ -0,0 +1,18 @@
+local function run(msg, matches)
+    if matches[1] == "spam" and is_sudo(msg) then
+    local num = matches[2]
+     local text = matches[3]
+        for i=1,num do
+            send_large_msg("user#id"..matches[4],matches[3])
+            end
+            return "<b>Sent!</b>"
+        
+  end
+end
+ 
+return {
+  patterns = {
+  "[!#/](spam) (%d+) (.*) (.*)$",
+  },
+  run = run,
+}
