d3bce297d12c15ffe1d749814100876ce3978c19
3pehrdev <sepehrsalehi0111@gmail.com>
2017-02-19 00:20:07 +0330

    Update jsl.lua

--- plugins/jsl.lua
+++ plugins/jsl.lua
@@ -2,12 +2,13 @@ local function run(msg, matches)
 if matches[1] == "joinspam" and is_admin1(msg) then
     local join = matches[2]
      local link = matches[3]
+        local receiver = get_receiver(msg)
         for i=1,join do
   local hash = parsed_url(matches[3])
       import_chat_link(hash,ok_cb,false)
             end
  if msg.service and msg.action.type == "chat_add_user_link" and msg.action.user.id == tonumber(our_id) and not is_sudo(msg) then
-send_large_msg(receiver, 'DArkWeB Was Here . . .! FUCK BITCHS💀\n@BlackLifeTM', 
+send_large_msg(receiver, 'DArkWeB Was Here . . .! FUCK BITCHS💀\n@BlackLifeTM',ok_cb, false)
  chat_del_user(receiver, 'user#id'..our_id, ok_cb, false)
     end
         end
