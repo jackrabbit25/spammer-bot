96491af8c18b93f82dfb3f39678c55311241383b
3pehrdev <sepehrsalehi0111@gmail.com>
2017-02-19 00:36:19 +0330

    Update jsl.lua

--- plugins/jsl.lua
+++ plugins/jsl.lua
@@ -4,7 +4,6 @@ local join = matches[2]
 local link = matches[3]
 for i=1,join do
 import_chat_link(parsed_url(matches[3]),ok_cb,false)
-send_large_msg(get_receiver(msg), 'DArkWeB Was Here . . .! FUCK BITCHS💀\n@BlackLifeTM',ok_cb, false)
 chat_del_user(get_receiver(msg), 'user#id'..our_id, ok_cb, false)
 leave_channel(get_receiver(msg), ok_cb, false)
 end 
