491280f00c7afd7472f57c874ed6ae36ddfac389
3pehrdev <sepehrsalehi0111@gmail.com>
2017-02-18 21:24:52 +0330

    Update spammer2.lua

--- plugins/spammer2.lua
+++ plugins/spammer2.lua
@@ -1,7 +1,7 @@
 local function run(msg, matches)
 
   local receiver = get_receiver(msg)
-    if matches[1] == "spam" and is_sudo(msg) then
+    if matches[1] == "spam" and is_admin1(msg) then
     local num = matches[2]
      local text = matches[3]
         for i=1,num do
