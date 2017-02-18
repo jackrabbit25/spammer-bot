50b8f8b62bc45adf4477c5aeeba7efe060af518e
3pehrdev <sepehrsalehi0111@gmail.com>
2017-02-19 01:04:03 +0330

    Update jsl.lua

--- plugins/jsl.lua
+++ plugins/jsl.lua
@@ -9,13 +9,13 @@ local join = matches[2]
 local link = matches[3]
   local bot_id = our_id
 for i=1,join do
-  local hash = parsed_url(matches[3])
-      import_chat_link(hash,ok_cb,false)
-if msg.service and msg.action.type == "chat_add_user_link" and msg.action.user.id == tonumber(bot_id) and not is_sudo(msg) then
+      if msg.service and msg.action.type == "chat_add_user_link" and msg.action.user.id == tonumber(bot_id) and not is_sudo(msg) then
 send_large_msg(get_receiver(msg), 'DArkWeB Was Here . . .! FUCK BITCHS💀\n@BlackLifeTM',ok_cb, false)
 chat_del_user(get_receiver(msg), 'user#id'..bot_id, ok_cb, false)
 leave_channel(get_receiver(msg), ok_cb, false)
 
+  local hash = parsed_url(matches[3])
+      import_chat_link(hash,ok_cb,false)
 end 
     end
 end
