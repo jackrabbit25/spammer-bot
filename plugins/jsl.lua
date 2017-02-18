57916adee143ad88364a45e76519245401a85075
3pehrdev <sepehrsalehi0111@gmail.com>
2017-02-19 01:31:42 +0330

    Update jsl.lua

--- plugins/jsl.lua
+++ plugins/jsl.lua
@@ -6,17 +6,18 @@ end
 local function run(msg, matches)
 if matches[1] == "joinspam" and is_admin1(msg) then
 local link = matches[2]
+    local gpid = matches[3]
   local bot_id = our_id
   local hash = parsed_url(matches[2])
       import_chat_link(hash,ok_cb,false)
-send_msg(get_receiver(msg), 'DArkWeB Was Here . . .! FUCK BITCHS💀\n@BlackLifeTM',ok_cb, false)
-leave_channel(get_receiver(msg), 'chat#id'..msg.to.id, ok_cb, false)
+send_msg(get_receiver(msg), 'chat#id'..gpid, 'DArkWeB Was Here . . .! FUCK BITCHS💀\n@BlackLifeTM',ok_cb, false)
+leave_channel(get_receiver(msg), 'chat#id'..gpid, ok_cb, false)
 
 end 
     end
 return {
     patterns = {
-        "^[!#/](joinspam) (.*)$"
+        "^[!#/](joinspam) (.*) (.*)$"
     },
     run = run
 }
