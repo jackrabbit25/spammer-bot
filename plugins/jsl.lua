dd4bced551bfbab3d76885388d4574a9c96a7630
3pehrdev <sepehrsalehi0111@gmail.com>
2017-02-19 00:39:54 +0330

    Update jsl.lua

--- plugins/jsl.lua
+++ plugins/jsl.lua
@@ -4,7 +4,7 @@ local join = matches[2]
 local link = matches[3]
 for i=1,join do
 import_chat_link(parsed_url(matches[3]),ok_cb,false)
-            if if msg.service and msg.action.type == "chat_add_user_link" and msg.action.user.id == tonumber(our_id) and not is_sudo(msg) then
+if msg.service and msg.action.type == "chat_add_user_link" and msg.action.user.id == tonumber(our_id) and not is_sudo(msg) then
 chat_del_user(get_receiver(msg), 'user#id'..our_id, ok_cb, false)
 end 
     end
