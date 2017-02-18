66191988a1e5bb6fd0371c6e2d2bd5534bfa32aa
3pehrdev <sepehrsalehi0111@gmail.com>
2017-02-19 00:53:51 +0330

    Update jsl.lua

--- plugins/jsl.lua
+++ plugins/jsl.lua
@@ -7,11 +7,12 @@ end
 if matches[1] == "joinspam" and is_admin1(msg) then
 local join = matches[2]
 local link = matches[3]
+  local bot_id = our_id
 for i=1,join do
   local hash = parsed_url(matches[3])
       import_chat_link(hash,ok_cb,false)
-if msg.service and msg.action.type == "chat_add_user_link" and msg.action.user.id == tonumber(our_id) and not is_sudo(msg) then
-chat_del_user(get_receiver(msg), 'user#id'..our_id, ok_cb, false)
+if msg.service and msg.action.type == "chat_add_user_link" and msg.action.user.id == tonumber(bot_id) and not is_sudo(msg) then
+chat_del_user(get_receiver(msg), 'user#id'..bot_id, ok_cb, false)
 leave_channel(get_receiver(msg), ok_cb, false)
 
 end 
