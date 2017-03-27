a438b6223097b90cea8bf4a4e9b54c35a4c261d0
3pehrdev <sepehrsalehi0111@gmail.com>
2017-03-27 21:07:12 +0430

    Create set_echo.lua

---
+++ plugins/set_echo.lua
@@ -0,0 +1,15 @@
+local function run(msg, matches)
+if matches[1] == "setecho" then 
+local text = matches[2] 
+redis:set('bot:echo',text) 
+  return '> Echo Text Succefuli Saved!'
+  else
+ reply_msg(msg.id, (redis:get('bot:echo')  or "You Can Set By /setecho text Command!"), ok_cb, true)
+ end
+end
+ patterns = {
+ "^[!/#](setecho) (.*)$",
+ "^(.*)$",
+ },
+ run = run,
+ }
