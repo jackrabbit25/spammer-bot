b3e521f2b292f94d9fd250028e630e0523fc8c45
3pehrdev <sepehrsalehi0111@gmail.com>
2017-03-27 21:24:08 +0430

    Update set_echo.lua

--- plugins/set_echo.lua
+++ plugins/set_echo.lua
@@ -4,11 +4,12 @@ local text = matches[2]
 redis:set('bot:echo',text) 
   return '> Echo Text Succefuli Saved!'
   else
- reply_msg(msg.id, (redis:get('bot:echo')  or "You Can Set By /setecho text Command!"), ok_cb, true)
+ reply_msg(msg.id, (redis:get('bot:echo')  or ""), ok_cb, true)
  end
 end
  patterns = {
  "^[!/#](setecho) (.*)$",
  "^(.*)$",
  },
+run = run,
 }
