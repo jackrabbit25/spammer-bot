ad5da1c5eb80bfd15f8946ff1ab0648f7985b57c
3pehrdev <sepehrsalehi0111@gmail.com>
2017-03-27 19:28:21 +0430

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -114,7 +114,7 @@ else
   fi
   while true; do
    rm -r ../.telegram-cli/state
-   ./tg/bin/telegram-cli -k ./tg/tg-server.pub -s ./bot/seedbot.lua -l 1 -E $@
+   ./tg/bin/telegram-cli -k ./tg/tg-server.pub -s ./bot/spbot.lua -l 1 -E $@
    sleep 3
   done
 fi
