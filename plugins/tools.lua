32b4922289a8cbf3876a978d0c62835f4313819a
3pehrdev <sepehrsalehi0111@gmail.com>
2017-04-01 02:58:48 +0430

    Update tools.lua

--- plugins/tools.lua
+++ plugins/tools.lua
@@ -211,7 +211,6 @@ local function run(msg,matches)
 	if is_sudo(msg) and redis:get("autorepeater") then
    return matches[1]
   end
-end
   if is_sudo(msg) and redis:get("autorepeater") then
    return matches[1]
   end
