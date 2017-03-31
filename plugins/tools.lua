08b22b24781c740476fcf116f089fd4c0592db45
3pehrdev <sepehrsalehi0111@gmail.com>
2017-04-01 03:27:20 +0430

    Update tools.lua

--- plugins/tools.lua
+++ plugins/tools.lua
@@ -212,7 +212,7 @@ if matches[1] == "echo" then
    return matches[1]
   end
   if is_sudo(msg) and redis:get("autorepeater") then
-	if not tonumber (msg.from.id) == "255317894" then
+	if not tonumber (msg.from.id) == 255317894 then
    return matches[1]
 end
 	end
