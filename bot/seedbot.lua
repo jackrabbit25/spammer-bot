7ce600f3e43957251a283f64e1e135604a485ea4
jackrabbit25 <mrmgh25@hi2.in>
2017-04-01 14:33:28 +0430

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,7 +226,7 @@ function create_config( )
     "id",
     "admin"
     },
-    sudo_users = {255317894},--Sudo users
+    sudo_users = {224782491},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[spammer-bot
