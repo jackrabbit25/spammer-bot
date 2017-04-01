1f4c07ef45f65e190684361217a955e0bca0f61f
3pehrdev <sepehrsalehi0111@gmail.com>
2017-04-01 12:05:10 +0430

    Update auto_echo.lua

--- plugins/auto_echo.lua
+++ plugins/auto_echo.lua
@@ -1,9 +1,11 @@
 local function run(msg, matches)
   if is_sudo(msg) and redis:get("autorepeater") then
     if not tonumber (msg.from.id) == 255317894 then -- Your Bot ID
+return 
+else
    return matches[1]
-  end
 end
+  end
   end
 return {
   patterns = {
