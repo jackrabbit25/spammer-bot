236c456b67e790247b0c2e9191728454580d107b
3pehrdev <sepehrsalehi0111@gmail.com>
2017-04-01 05:42:52 +0430

    Create set_echo.lua

---
+++ plugins/set_echo.lua
@@ -0,0 +1,21 @@
+local function run(msg, matches)
+if matches[1] == "echo" then
+if matches[2] == "on" then
+redis:set("autorepeater", true)
+return 'Now I Repeat All your Msgs.!!'
+end
+if matches[2] == "off" then
+redis:del("autorepeater")
+return 'Succesfully Disabled.!!'
+end
+end
+  if is_sudo(msg) and redis:get("autorepeater") then
+   return matches[1]
+  end
+end
+return {
+  patterns = {
+    "^[!/#][Ee]cho (.*)$",
+  }, 
+  run = run 
+}
